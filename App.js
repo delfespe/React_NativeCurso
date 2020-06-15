302be658716887c5c6993fe9583a3d10756b3214
delfin <delfinespejod@gmail.com>
2020-06-15 11:05:38 -0500

    temporal

--- App.js
+++ App.js
@@ -1,22 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./app/navigations/Navigation";
 
 export default function App() {
-  const nombre = "Agustin";
-  const apellido = "Navarro";
-
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <Navigation></Navigation>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
