c2ce0c395abb7bc62cf092846b21aaf7a29f6a7e
Delfin Espejo Donayre <despejod@bcp.com.pe>
2020-06-16 10:46:19 -0500

    Revert "temporal"
    This reverts commit 302be658716887c5c6993fe9583a3d10756b3214.

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React from "react";
-import Navigation from "./app/navigations/Navigation";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return <Navigation></Navigation>;
+  const nombre = "Agustin";
+  const apellido = "Navarro";
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
