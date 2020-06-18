2f426197befecbe2e9ed291face6d6fcd7cb01a3
delfin <delfinespejod@gmail.com>
2020-06-18 15:09:29 -0500

    React Native Elements: iconos

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -8,12 +8,23 @@ import TopRestauranrStack from "../navigations/TopRestauranrStack";
 import SearchStack from "../navigations/SearchStack";
 import AccountStack from "../navigations/AccountStack";
 
+import { Icon } from "react-native-elements";
+
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="restaurants"
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
         <Tab.Screen
           name="restaurants"
           component={RestaurantStack}
@@ -42,4 +53,28 @@ export default function Navigation() {
       </Tab.Navigator>
     </NavigationContainer>
   );
+}
+
+function screenOptions(route, color) {
+  let iconName;
+  switch (route.name) {
+    case "restaurants":
+      iconName = "compass-outline";
+      break;
+    case "favorites":
+      iconName = "heart-outline";
+      break;
+    case "toprestaurants":
+      iconName = "start-outline";
+      break;
+    case "search":
+      iconName = "magnify";
+      break;
+    case "account":
+      iconName = "home-outline";
+      break;
+    default:
+      break;
+  }
+  return <Icon type="material-community" name={iconName} color={color}></Icon>;
 }
